d7e356b662e0292bf1aa670147bc7f7af1cbd67e
hudson <huds0nben2007@gmail.com>
2024-12-05 12:11:41 -0600

    got part 2

--- day2part2.py
+++ day2part2.py
@@ -5,31 +5,49 @@ for line in f:
     for x in range(len(row)):
         row[x] = int(row[x])
     data.append(row)
+
 f.close()
+
 
 counter = 0
 
-for x in data:
+
+def test_unsafe(x): # returns a boolean, if true then unsafe, if false then safe
     increasing = False
     decreasing = False
-    unsafe = False
     for y in range(len(x) - 1):
         if abs(x[y]-x[y+1]) > 3:
-            unsafe = True
-            break
-
+            return True
+            
         if x[y] < x[y+1]:
             increasing = True
         elif x[y] > x[y+1]:
             decreasing = True
         elif x[y] == x[y+1]:
-            unsafe = True
-            break
+            return True
+    if (increasing and decreasing):
+        return True
+    else:
+        return False
+    
+            
+def safe_with_damping(list):
+    if not test_unsafe(list):
+        return True
+    
+    for i in range(len(list)):
+        temp = list[:i] + list[i+1:]
+        if not test_unsafe(temp):
+            return True
         
-    if unsafe or (increasing and decreasing):
+    return False
+
+for x in data:
+
+    if safe_with_damping(x):
         counter += 1
 
-print(1000-counter)
+print(counter)
 
 # bro im so slow i put the unsafe checked inside of the fucking y loop not the x loop
 
