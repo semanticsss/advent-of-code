e91de1c8ade8bc8525c2aaf4068fa118676f7ac9
hudson <huds0nben2007@gmail.com>
2024-12-06 09:13:43 -0600

    new stuff

--- day3part1.py
+++ day3part1.py
@@ -1,25 +1,28 @@
-f = open('day3set.txt', 'r')
-data = f.read()
-
-counter = 0
-
-def search_for_valid_strings(data):
-    while len(data) > 1:
-        counter = 0
-        x = data.find("mul(") # x is the position of the mul( string bullshit
-        data = data[x:]
-        print(data)
-        comma_index = data.index(",")
-        paren_index = data.index(")")
-        if data[(x+5):comma_index].isdigit() and data[(comma_index + 1):paren_index].isdigit():
-            number1 = int(data[(x+5):comma_index])
-            number2 = int(data[(comma_index + 1):paren_index])
-            k = number1 * number2
-            data = data[:paren_index]
-            counter += k
-    print(counter)
-
-search_for_valid_strings(data)
+#why was i not told about regex buddy im gonna crash out this is crazy
 
 
+import regex as re
 
+with open('day3set.txt', 'r') as f:
+    data = f.read()
+
+f.close()
+
+def search_for_valid_string(data):
+
+    counter = 0
+
+    idk_whatever_mul_is_or_something = r"mul\((\d{1,3}),(\d{1,3})\)" #this is so nuts and broken this gotta get patched insta
+
+    all_valid = re.findall(idk_whatever_mul_is_or_something, data)
+
+    for match in all_valid:
+        first_number = int(match[0])
+        second_number = int(match[1])
+        counter += first_number * second_number
+        
+
+    return counter
+
+
+print(search_for_valid_string(data))
